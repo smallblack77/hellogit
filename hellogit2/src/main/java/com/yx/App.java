da952e57a02c4fc587cb7809c2e4da06f07555a8
smallblack77 <1485433278@qq.com>
2021-03-30 17:48:17 +0800

    分支添加

--- hellogit2/src/main/java/com/yx/App.java
+++ hellogit2/src/main/java/com/yx/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("更新1");
         System.out.println("分支1");
+        System.out.println("分支添加");
     }
 }
